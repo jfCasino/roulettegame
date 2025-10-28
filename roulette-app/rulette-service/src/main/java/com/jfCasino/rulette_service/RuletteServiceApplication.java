e3d798bbeceea761550e239d68576032ed0c54ac
Jaka Furlan <jaka.furlan@cosylab.com>
2025-10-28 19:23:02 +0100

    initialised spring boot

---
+++ roulette-app/rulette-service/src/main/java/com/jfCasino/rulette_service/RuletteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jfCasino.rulette_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuletteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuletteServiceApplication.class, args);
+	}
+
+}
