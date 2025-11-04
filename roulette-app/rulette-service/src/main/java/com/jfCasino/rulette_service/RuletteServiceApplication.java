c358ece3d27834ba45b118f09b993928a6e0e557
Jaka Furlan <jaka.furlan@cosylab.com>
2025-11-04 20:46:05 +0100

    created parent pom, changed child poms

--- roulette-app/rulette-service/src/main/java/com/jfCasino/rulette_service/RuletteServiceApplication.java
+++ roulette-app/rulette-service/src/main/java/com/jfCasino/rulette_service/RuletteServiceApplication.java
@@ -2,8 +2,10 @@ package com.jfCasino.rulette_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//TODO remove exclude when db is configured		
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RuletteServiceApplication {
 
 	public static void main(String[] args) {
